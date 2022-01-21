b67e7d2ad97807056abe09c3a665c592d990983b
bfranceschin <bfranceschin@gmail.com>
2022-01-21 13:33:44 -0300

    breed user interface

--- client/index.js
+++ client/index.js
@@ -3,16 +3,16 @@ var web3 = new Web3(Web3.givenProvider);
 var instance;
 var user;
 var contractAddress = "0x0BE48330fE09E24b32B0babb4df9f1786BcB3bbC";
+var userKitties = [];
 var userTokens = [];
 
-$(document).ready(function(){
+var selectedMother
+var selectedFather
 
+$(document).ready(function(){
     window.ethereum.enable().then(function(accounts){
         instance = new web3.eth.Contract(abi, contractAddress, { from: accounts[0]})
         user = accounts[0];
-
-        console.log(instance);
-
         initCollection();
     }).then(function(){
         instance.events.Birth({}, function(error, event){ console.log(event); })
@@ -26,7 +26,7 @@ $(document).ready(function(){
             event.returnValues.genes + ", owner: " + event.returnValues.owner + ".";
             console.log(alertstring);
             alert(alertstring);
-            //console.log(event.returnValues);
+            location.reload()
         })
         .on('changed', function(event){
             // nothing to do yet
@@ -37,34 +37,69 @@ $(document).ready(function(){
     });
 })
 
-function initCollection () {
-    if (user) {
-        instance.methods.totalSupply().call().then(function(result) {
-            //console.log(result, web3.utils.isBN(result), typeof result, parseInt(result));
-            let totalSupply = parseInt(result)
-            let owners = []
-            for (i=0; i<totalSupply; ++i) {
-                owners.push(instance.methods.ownerOf(i).call())
-            }
-            Promise.all(owners).then((values) => {
-                console.log(values)
-                let userTokens = [...Array(values.length).keys()].filter( (token) => {
-                    //console.log("no filter", token, values[token], user, values[token].toLowerCase()===user.toLowerCase())
-                    return values[token].toLowerCase() == user.toLowerCase()
-                } )
-                console.log(userTokens)
-                Promise.all(userTokens.map( (token) => {return instance.methods.getKitty(token).call() } )).then ( (kitties) => {
-                    console.log(kitties)
-                    userTokens = kitties
-                    if (document.getElementById('userCatList')) {
-                        $('#userCatList').empty()
-                        userTokens.forEach( (token) => {
-                            //token
-                            appendCat(token.genes);
-                        })
-                    }
-                })
-            })
-        })
+function selectFather (catId) {
+  $('#fatherDropDownButton').text(`Cat ${catId}`)
+  selectedFather = catId
+}
+
+function selectMother (catId) {
+  $('#motherDropDownButton').text(`Cat ${catId}`)
+  selectedMother = catId
+}
+
+function breed () {
+  let mother = $('#motherDropDownButton').prop("catId")
+  let father = $('#fatherDropDownButton').prop("catId")
+  console.log("Breeeeeeed", selectedMother, selectedFather)
+
+  console.log(instance.methods)
+  
+  instance.methods.breed(selectedMother, selectedFather).send({}, function (error, txHash){
+    if (error)
+      console.log(error)
+    else {
+      console.log("transaction hash: " + txHash)
     }
+  })
+
+}
+
+function initCollection () {
+  if (user) {
+    // get total supply
+    instance.methods.totalSupply().call().then(function(result) {
+      let totalSupply = parseInt(result)
+      // get each token owner
+      let owners = []
+      for (i=0; i<totalSupply; ++i) {
+        owners.push(instance.methods.ownerOf(i).call())
+      }
+      Promise.all(owners).then((values) => {
+        // filter user tokens
+        userTokens = [...Array(values.length).keys()].filter( (token) => {
+          return values[token].toLowerCase() == user.toLowerCase()
+        } )
+        console.log(userTokens)
+        Promise.all(userTokens.map( (token) => {return instance.methods.getKitty(token).call() } )).then ( (kitties) => {
+          console.log(kitties)
+          userKitties = kitties
+          if (document.getElementById('userCatList')) {
+            $('#userCatList').empty()
+            userKitties.forEach( (kitty) => {
+              appendCat(kitty.genes)
+            })
+          }
+          if (document.getElementById("motherBreedDropdown")) {
+            $('#motherBreedDropdown').empty()
+            $('#fatherBreedDropdown').empty()
+            for (let i=0; i<userTokens.length; i++) {
+              let tokenId = userTokens[i]
+              $('#motherBreedDropdown').append(`<a cat=${tokenId} class="dropdown-item" onclick=selectMother(${tokenId}) >Cat ${tokenId}</a>`);
+              $('#fatherBreedDropdown').append(`<a cat=${tokenId} class="dropdown-item" onclick=selectFather(${tokenId}) >Cat ${tokenId}</a>`);
+            }
+          }
+        })
+      })
+    })
+  }
 }
